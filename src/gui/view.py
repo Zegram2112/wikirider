72f19cd17f304001eff329531624f4a5df0c5dfa
Nicolás <nicolas.lemunir2112@gmail.com>
2018-08-15 21:18:47 -0300

    Update view from PySide to PySide2

--- src/gui/view.py
+++ src/gui/view.py
@@ -1,55 +1,89 @@
 import sys
-from PySide import QtCore, QtGui, QtWebKit
+from PySide2 import QtCore, QtWidgets, QtGui, QtWebEngineWidgets
+from PySide2.QtWebEngineWidgets import QWebEngineView
 
 
-class Window(QtGui.QMainWindow):
+class RiderWindow(QtWidgets.QMainWindow):
     """WikiRider Main Window"""
 
     def __init__(self):
-        super(Window, self).__init__()
+        super(RiderWindow, self).__init__()
         self.setGeometry(100, 100, 1024, 768)
         self.setWindowTitle("WikiRider")
-        self.ui = RideUi()
+        self._create_components()
+        self._set_connections()
+
+    def _create_components(self):
+        self.tool_bar = RiderToolBar()
+        self.rider_view = RiderWebView()
+        self.status_bar = QtWidgets.QStatusBar()
         self.setCentralWidget(self.ui)
+        self.addToolBar(self.tool_bar)
+        self.setStatusBar(QtWidgets.QStatusBar())
+
+    def _set_connections(self):
+        self.tool_bar.ride_pressed.connect(self.ui.ride)
+        self.rider_view.urlChanged.connect(self.tool_bar.change_url)
 
 
-class RideUi(QtGui.QWidget):
+class RiderWebView(QtWebEngineWidgets.QWebEngineView):
     """Central widget"""
 
     def __init__(self):
-        super(RideUi, self).__init__()
-        self.web_view = QtWebKit.QWebView()
-        self.web_view.load(
+        super(RiderUi, self).__init__()
+        self.load(
             QtCore.QUrl("https://en.wikipedia.org/wiki/Main_Page"))
-        self.top_bar = TopBar()
-        layout = QtGui.QVBoxLayout()
-        layout.addWidget(self.top_bar)
-        layout.addWidget(self.web_view)
-        self.setLayout(layout)
+
+    @QtCore.Slot(QtCore.QUrl)
+    def ride(self, url):
+        self.load(url)
+        # TODO start wikirider
 
 
-class TopBar(QtGui.QWidget):
-    """Bar containing search"""
+class RiderToolBar(QtWidgets.QToolBar):
+    """Bar with the most useful tools needed to ride Wikipedia"""
+
+    ride_pressed = QtCore.Signal(QtCore.QUrl)
 
     def __init__(self):
-        super(TopBar, self).__init__()
-        self.ride_btn = QtGui.QCommandLinkButton("Ride!")
-        # self.ride_btn.resize(100, 30)
-        self.url_entry = QtGui.QLineEdit()
-        self.depth_entry = QtGui.QLineEdit("0")
+        super(RiderToolBar, self).__init__()
+        self.setIconSize(QtCore.QSize(32, 32))
+        self._create_components()
+        self._set_connections()
+
+    def _create_components(self):
+        self.ride_actn = self.addAction(
+            QtGui.QIcon("assets/horse-riding"),
+            "Ride!")
+        self.address_bar = QtWidgets.QLineEdit()
+        self.depth_entry = QtWidgets.QLineEdit("0")
         self.depth_entry.setValidator(QtGui.QIntValidator())
         self.depth_entry.setMaximumSize(200, 16777215)
-        layout = QtGui.QHBoxLayout()
-        layout.addWidget(self.ride_btn)
-        layout.addWidget(self.url_entry)
-        layout.addWidget(self.depth_entry)
-        self.setLayout(layout)
-        self.setMaximumSize(16777215, 60)
+        self.addWidget(self.url_entry)
+        self.addWidget(self.depth_entry)
 
+    def _set_connections(self):
+        self.ride_actn.triggered.connect(self.on_ride_press)
+
+    def on_ride_press(self):
+        self.ride_pressed.emit(self.current_url)
+
+    @property
+    def current_url(self):
+        """Get current url from the address bar"""
+        url = QtCore.QUrl()
+        url.setUrl(self.url_entry.text())
+        return url
+
+    @QtCore.Slot(QtCore.QUrl)
+    def change_url(self, url):
+        """Change url from the address bar"""
+        self.address_bar.setText(url.toString())
 
 # Test purposes only
 if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
-    window = Window()
+    app = QtWidgets.QApplication(sys.argv)
+    app.setStyleSheet("QToolBar { spacing: 10px; padding: 10px; }")
+    window = RiderWindow()
     window.show()
     app.exec_()
