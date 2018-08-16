1501ff173a5826de933c5dce48a1af9d8782e3d5
Zegram2112 <nicolas.lemunir2112@gmail.com>
2018-08-16 02:53:13 -0300

    Fix qt not running on Ubuntu 17.10
    Fixed bad loading of GL library by PySide on debian-based distros.
    Information can be found here: https://bugs.launchpad.net/ubuntu/+source/python-qt4/+bug/941826/)

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) >= 2 and sys.argv[1] == 'gui':
+        # - Fix qt loading wrong GL library under linux -------------------
+        if sys.platform.startswith( 'linux' ) :
+            import ctypes.util
+            ctypes.CDLL(ctypes.util.find_library('GL'), ctypes.RTLD_GLOBAL)
+        # -----------------------------------------------------------------
         app = QtApp()
     else:
         app = TerminalApp()
