039ec44e64f27341d53062ac33a44bf948863e71
Nicolás <nicolas.lemunir2112@gmail.com>
2018-08-28 00:38:49 -0300

    Update RiderWindow and WikiRiderWorker
    This change was made because the QThread in which WikiRiderWorker runned 
    was started at the initialization of RiderWindow and it was never 
    finished. Now the worker and his thread are created only when the 
    "Ride!" button is clicked and are deleted when the worker finish his 
    job.
    
    Also a few docstrings were added.

--- src/qt/view/rider_window.py
+++ src/qt/view/rider_window.py
@@ -1,23 +1,23 @@
 import sys
 from PySide2.QtWidgets import QMainWindow, QStatusBar, QDockWidget
-from PySide2.QtCore import Qt, Slot, QUrl, QThread
+from PySide2.QtCore import Qt, Slot, QUrl, QThread, Signal
 from src.qt.view import RiderToolBar, RiderWebView, RiderUrlList, WikiRiderWorker
 
 
 class RiderWindow(QMainWindow):
     """WikiRider Main Window"""
 
+    worker_activated = Signal(QUrl)
+
     def __init__(self):
         super(RiderWindow, self).__init__()
+
         self.setGeometry(100, 100, 1024, 768)
         self.setWindowTitle("WikiRider")
-        self.worker = WikiRiderWorker()
-        self.worker_thread = QThread()
-        self.worker.moveToThread(self.worker_thread)
-        self.worker_thread.start()
         self._create_components()
         self._create_dock_components()
         self._set_connections()
+
         starting_url = QUrl("https://en.wikipedia.org/wiki/Main_Page")
         self.rider_view.load(starting_url)
 
@@ -39,7 +39,23 @@ class RiderWindow(QMainWindow):
     def _set_connections(self):
         self.rider_view.urlChanged.connect(self.tool_bar.change_url)
         self.url_list.url_clicked.connect(self.rider_view.change_url)
-        self.tool_bar.ride_clicked.connect(self.worker.ride)
-        self.worker.started.connect(self.url_list.clear)
+        self.tool_bar.ride_clicked.connect(self.activate_worker)
+
+    @Slot(QUrl)
+    def activate_worker(self, url):
+        """Activates a WikiRiderWorker instance running in an isolated QThread"""
+        self.worker = WikiRiderWorker()
+        self.worker_thread = QThread()
+        self.worker.moveToThread(self.worker_thread)
+        # Worker UI connections
+        self.worker.ride_started.connect(self.url_list.clear)
         self.worker.url_visited.connect(self.rider_view.change_url)
         self.worker.url_visited.connect(self.url_list.add_url)
+        # Worker flow connections
+        self.worker_activated.connect(self.worker.ride)
+        self.worker.ride_finished.connect(self.worker_thread.quit)
+        self.worker.ride_finished.connect(self.worker_thread.deleteLater)
+        self.worker_thread.finished.connect(self.worker_thread.deleteLater)
+
+        self.worker_thread.start()
+        self.worker_activated.emit(url)
--- src/qt/view/wikirider_worker.py
+++ src/qt/view/wikirider_worker.py
@@ -5,16 +5,21 @@ from src.core import WikiRider
 class WikiRiderWorker(QObject):
 
     url_visited = Signal(QUrl)
-    started = Signal()
-    finished = Signal()
+    ride_started = Signal()
+    ride_finished = Signal()
 
     def __init__(self):
         super(WikiRiderWorker, self).__init__()
 
     @Slot(QUrl)
     def ride(self, url):
-        self.started.emit()
+        """Creates a WikiRider instance and make it run from a starting url"""
+        self.ride_started.emit()
         rider = WikiRider(url.toString(), 5)
         for rider_state in rider.run():
             self.url_visited.emit(QUrl(rider_state.visited_urls[-1]))
-        self.finished.emit()
+        self.ride_finished.emit()
+
+    @Slot()
+    def stop(self):
+        self.ride_finished.emit()
